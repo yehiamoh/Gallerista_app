9ce1598be257994f9568106f9e3e2f2d67c130a1
Yehia Mohamed <yehiamohamed915@yahoo.com>
2024-10-27 22:35:33 +0300

    api key

--- index.ts
+++ index.ts
@@ -4,6 +4,7 @@ import bodyParser from "body-parser";
 import dotenv from 'dotenv';
 import authRouter from "./src/routes/auth_routes";
 import errorHandler from "./src/services/error_handler";
+import {apiKeyAuth} from "./src/util/api_key"
 dotenv.config();
 
 const port =process.env.PORT||8080;
@@ -12,7 +13,7 @@ function start(){
    try{
       const app=express();
       app.use(bodyParser.json());
-      app.use("/api/V0",authRouter);
+      app.use("/api/V0",apiKeyAuth,authRouter);
       app.get('/api/V0/dummy',(req,res)=>{
          res.json({
             message:"for fady"
---
+++ src/util/api_key.ts
@@ -0,0 +1,10 @@
+import doetenv from "dotenv";
+doetenv.config();
+import { Request,Response,NextFunction } from "express"
+ export const apiKeyAuth=(req,res,next)=>{
+   const apiKey=req.header("x-api-key");
+   if(!apiKey||apiKey!==process.env.API_Key){
+      return res.status(403).json({ message: 'Forbidden: Invalid API key' });  
+   }
+   next();
+}
