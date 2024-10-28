ef40267142cecd5e9b2cf4812d7c59db7b2f03fb
Yehia Mohamed <yehiamohamed915@yahoo.com>
2024-10-28 17:57:01 +0300

    trying to fix env variable error pt 3

--- src/util/api_key.ts
+++ src/util/api_key.ts
@@ -3,7 +3,7 @@ doetenv.config();
 import { Request,Response,NextFunction } from "express"
  export const apiKeyAuth=(req,res,next)=>{
    const apiKey=req.header("x-api-key");
-   if(!apiKey||apiKey!==process.env.API_Key){
+   if(!apiKey||apiKey!==process.env.API_KEY){
       return res.status(403).json({ message: 'Forbidden: Invalid API key' });  
    }
    next();
