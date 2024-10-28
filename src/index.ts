d8ee4d75abf499993f995ed1de60f8591c6381d3
Yehia Mohamed <yehiamohamed915@yahoo.com>
2024-10-28 13:35:18 +0300

    setting up vercel

--- src/index.ts
+++ src/index.ts
@@ -9,14 +9,13 @@ dotenv.config();
 
 const port =process.env.PORT||8080;
 
-function start(){
-   try{
+
       const app=express();
       app.use(bodyParser.json());
       app.use("/api/V0",apiKeyAuth,authRouter);
       app.get('/api/V0/dummy',(req,res)=>{
          res.json({
-            message:"for fady"
+            message:"tst1"
          });
       })
 
@@ -26,9 +25,4 @@ function start(){
       app.listen(port,()=>{
          console.log(`server is running on port : ${port}`)
       });
-   }
-   catch(error:any){
-      console.log(`error in running server \n ${error.toString()}`)
-   }
-}
-start();
+export default app;
