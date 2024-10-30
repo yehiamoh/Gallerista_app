a19a8a801aa926688333ca0aad7bf6c7331786fb
Yehia Mohamed <yehiamohamed915@yahoo.com>
2024-10-30 21:45:20 +0300

    trying

--- src/routes/user_routes.ts
+++ src/routes/user_routes.ts
@@ -7,6 +7,7 @@ import { UserController } from "../controllers/user_controller";
 const userRouter = express.Router();
 
 
+userRouter.use(ensureAuthentication);
 userRouter.get('/user/:id',UserController.getUserById);
 userRouter.get('/user/profile',UserController.getProfile);
 
