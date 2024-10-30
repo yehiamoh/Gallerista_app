d5cbdbe29e26345620a7b5ebb0ee75b9fcd3ed2e
Yehia Mohamed <yehiamohamed915@yahoo.com>
2024-10-30 20:49:40 +0300

    debugging the routes

--- src/routes/user_routes.ts
+++ src/routes/user_routes.ts
@@ -7,12 +7,7 @@ import { UserController } from "../controllers/user_controller";
 const userRouter = express.Router();
 
 
-
 userRouter.get('/user/:id',UserController.getUserById);
 userRouter.get('/user/profile',ensureAuthentication,UserController.getProfile);
-userRouter.post("/protected",(req,res)=>{
-   res.json("welcome to protected route")
-}); 
-
 
 export default userRouter;
