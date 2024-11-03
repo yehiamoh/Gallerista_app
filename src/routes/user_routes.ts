404ec609a6cdc15d5bfb8838d59d0a9492fe2614
Yehia Mohamed <yehiamohamed915@yahoo.com>
2024-11-04 01:03:27 +0200

    removing general middle ware in the user_router

--- src/routes/user_routes.ts
+++ src/routes/user_routes.ts
@@ -6,7 +6,6 @@ import { UserController } from "../controllers/user_controller";
 
 const userRouter = express.Router();
 
-userRouter.use(ensureAuthentication);
-userRouter.get('/user/profile',UserController.getProfile);
-userRouter.get('/user/:id/',UserController.getUserById);
+userRouter.get('/user/profile',ensureAuthentication,UserController.getProfile);
+userRouter.get('/user/:id/',ensureAuthentication,UserController.getUserById);
 export default userRouter;
