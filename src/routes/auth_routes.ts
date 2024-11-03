6862fe2a6f6b9b931a89c564de4cb7e240054b90
Yehia Mohamed <yehiamohamed915@yahoo.com>
2024-11-03 21:54:44 +0200

    changing auth_routes bug

--- src/routes/auth_routes.ts
+++ src/routes/auth_routes.ts
@@ -5,8 +5,8 @@ import upload from "../util/cloudinary-config";
 
 const authRouter = express.Router();
 
-authRouter.post("/auth/register",AuthController.register); 
-authRouter.post("/auth/login",upload.single("image"),AuthController.login); 
+authRouter.post("/auth/register",upload.single("image"),AuthController.register); 
+authRouter.post("/auth/login",AuthController.login); 
 //authRouter.get("/auth/refresh",AuthController.refresh);
 //authRouter.post("/auth/refresh",AuthController.refresh);
 
