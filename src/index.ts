600846166f8b1fa0de35bbf483f784e485ff6125
Yehia Mohamed <yehiamohamed915@yahoo.com>
2024-11-13 02:19:09 +0200

    adding more cors origind

--- src/index.ts
+++ src/index.ts
@@ -25,6 +25,10 @@ const corsOptions = {
       'https://gallery-git-main-fadys-projects-ce6c30fb.vercel.app',  
       'https://gallery-xh7a.vercel.app',
       'https://gallery-fadys-projects-ce6c30fb.vercel.app',
+      'https://gallery-gilt-kappa.vercel.app/?vercelToolbarCode=Bf2y6VJI3aOBcKT',
+      'https://gallery-git-main-fadys-projects-ce6c30fb.vercel.app/?vercelToolbarCode=PId5OuAjBPc9UlD',
+      'https://gallery-ln0zbjipd-fadys-projects-ce6c30fb.vercel.app/?vercelToolbarCode=23mKcphaOmjAoow',
+      'http://gallery-gilt-kappa.vercel.app',
       
 
       // Add any other frontend URLs that need access
