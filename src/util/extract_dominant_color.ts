5b0dbdc7a9c631674ef2c41c85fcdd5916bab5b4
Yehia Mohamed <yehiamohamed915@yahoo.com>
2024-11-14 19:19:11 +0200

    changing the width and height in the calulation for the dominant colour

--- src/util/extract_dominant_color.ts
+++ src/util/extract_dominant_color.ts
@@ -2,7 +2,7 @@ import vibrant from "node-vibrant";
 
 // Transform Cloudinary URL to use JPEG format and resize to 200x200 for faster processing
 const getCompatibleImageUrl = (url: string) => {
-  return url.replace("/upload/", "/upload/f_jpg,w_200,h_200,c_fill/");
+  return url.replace("/upload/", "/upload/f_jpg,w_300,h_300,c_fill/");
 };
 
 // Function to get dominant color from transformed URL
