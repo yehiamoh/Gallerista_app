510e823493783b4e1d79a46a529f7275eca3851a
Yehia Mohamed <yehiamohamed915@yahoo.com>
2024-11-14 16:49:43 +0200

    modify the url for the images to be compitable with the node-vibrant package

--- src/util/extract_dominant_color.ts
+++ src/util/extract_dominant_color.ts
@@ -1,8 +1,11 @@
 import vibrant from "node-vibrant";
-
+const getCompatibleImageUrl = (url: string) => {
+   return url.replace("/upload/", "/upload/f_jpg/");
+ };
 export const dominantColor =async(imagePath:string|undefined)=>{
    try{
-      const palette= await vibrant.from(imagePath).getPalette();
+      const compatibleImage=getCompatibleImageUrl(imagePath)
+      const palette= await vibrant.from(compatibleImage).getPalette();
       const dominantColor=palette.Vibrant.hex;
       return dominantColor || "#000000";
    }
